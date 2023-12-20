8a0c50e2d89fcfdff65919739093e38958e29562
JaeHun Kim <kkaa81@naver.com>
2023-12-20 21:36:06 +0900

    update metadata

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -3,12 +3,29 @@ import "swiper/css";
 import { Lang, getDictionary } from "../dictionaries";
 import Main from "./main";
 import StyledComponentsRegistry from "@/components/registry";
+import { Metadata } from "next";
 
 export interface Params {
   params: {
     lang: Lang;
   };
 }
+
+export const metadata: Metadata = {
+  title: "MSI",
+  description: "MSI",
+  alternates: {
+    canonical: "/",
+  },
+  openGraph: {
+    title: "MSI",
+    description: "MSI",
+    url: "/",
+    siteName: "MSI",
+    locale: "en-US",
+    type: "website",
+  },
+};
 
 export interface Props extends Params {
   children: React.ReactNode;
