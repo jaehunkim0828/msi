93d0406395844b1374c7e758e7c7a34054fe511e
JaeHun Kim <67530239+jaehunkim0828@users.noreply.github.com>
2023-12-20 09:34:48 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
