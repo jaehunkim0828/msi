ee5751f6caa4ad37c71f5df1759e5a04023899c7
JaeHun Kim <kkaa81@naver.com>
2023-12-21 23:38:36 +0900

    fix

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -257,7 +257,7 @@ export default function Page() {
                 MSI <strong>People</strong>
               </div>
               <div className={style.desc}>
-                <div>A taleted</div>
+                <div>A talented</div>
                 <div>and</div>
                 <div>dedicated</div>
                 <div>crew</div>
